28166e0885fb49e65e187c471fca0ae21d15e6c9
MAHESHTAPSE <maheshshankartapse5@gmail.com>
2025-02-12 13:58:46 +0530

    All Done 1St Commit

--- Program.cs
+++ Program.cs
@@ -13,7 +13,6 @@ builder.Services.AddDbContext<CrudContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
