72c2f88fed94015636dc57a581ef7d7e185e69c9
MAHESHTAPSE <maheshshankartapse5@gmail.com>
2025-02-12 13:58:00 +0530

    Add project files.

---
+++ Models/CrudContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductWeb.API.Models;
+
+public partial class CrudContext : DbContext
+{
+
+    public CrudContext(DbContextOptions<CrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+
+    public virtual DbSet<Product> Products { get; set; }
+
+   
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__categori__3214EC07C0EEE6F8");
+
+            entity.ToTable("categories");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC070CAB76F7");
+
+            entity.Property(e => e.Name).HasMaxLength(100);
+            entity.Property(e => e.Price)
+                .HasColumnType("decimal(10, 2)")
+                .HasColumnName("price");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .HasConstraintName("FK__Products__Catego__5EBF139D");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
